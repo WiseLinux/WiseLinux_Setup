aba07fe11a678c37f8ed1ee634cc370620c9aead
Jacob Atkins <jacobc2@gmail.com>
2010-01-12 13:30:41 -0500

    added to the start script

--- start.rb
+++ start.rb
@@ -9,14 +9,19 @@
 
 require 'yaml'
 
-`clear` # Lets start with a clean slate
+system("clear") # Lets start with a clean slate
 
 puts "This script will install several tools that are needed to run WiseLinux."
 puts "It will also configure your system for usage as a master node in a Cluster."
 puts "\n"
-puts "To continue press Enter\\Return..."
+puts "If there is an older config.yml file it will be overwritten."
+puts "Exit now and back it up if you wish to save it."
+puts "\n"
+puts "To continue press Enter\\Return to exit press ctrl+c ..."
 
 gets # Waiting for enter to be hit
+
+f = File.open("config.yml", 'w') # Open the config file after the user has contiuned
 
 puts "Rebuilding the system will optimize all under laying programs."
 puts "This task isn't required but is highly recomended."
@@ -25,14 +30,19 @@ puts "Would you like to rebuild the system? [Y/n]"
 
 system = gets.chomp.upcase
 
+# TODO There has to be a better way to write the config file rather than doing each little section at a time.
+# Start of the world rebuild
 while system != "Y" || system != "N"
+  if system == ""
+    system = "Y"
+  end
   if system == "Y"
     puts "The System will be rebuilt."
-    world = { :rebuild => true } 
+    config = { :world => { :rebuild => true } } 
     break
   elsif system == "N"
     puts "The System will not be rebuild."
-    world = { :rebuild => false }
+    config = { :world => { :rebuild => false } }
     break
   else
     puts "Please enter y or n..."
@@ -40,4 +50,35 @@ while system != "Y" || system != "N"
   end
 end
 
-File.open("config.yml", 'w') {|f| f.puts world.to_yaml }
+f.puts config.to_yaml # Write this section the file
+
+puts "\nWhat network device is pointing to the cluster?"
+
+lan_nic = gets.chomp
+
+puts "\nThe cluster will need a private subnet."
+puts "What will this range be? ex [192.168.0.0/16]"
+
+lan_subnet_range = gets.chomp
+
+puts "\nWhat will be the IP address of the network device pointing to the cluster?"
+
+lan_ip = gets.chomp
+
+config = { :lan => { :nic => lan_nic, :subnet => lan_subnet_range, :ip => lan_ip } }
+
+f.puts config.to_yaml
+
+# TODO Create real page with instructions on downloading MAUI
+puts "\n\nIt is requmented that you install the MAUI Cluster Scheduler."
+puts "MAUI is free but has to be downladed indepently, instructions"
+puts "on downloading MAUI can be found at http://something.com/wiki/maui"
+puts "\n"
+puts "Please enter the URL for your copy of MAUI or press enter to contiune without it..."
+
+maui_url = gets.chomp
+
+# TODO There needs to be a url validater here so that the script doesn't shit its pants down the road.
+config = { :maui => { :url => maui_url } }
+
+f.puts config.to_yaml
